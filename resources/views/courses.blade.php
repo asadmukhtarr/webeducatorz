ff99c2f7e56548b66bb4f4232fac6a952b59d316
axad03213 <axad03213@gmail.com>
2022-07-27 18:44:37 -0700

    images fixes

--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -426,8 +426,8 @@
             <div class="col-lg-4 responsive-column-half">
                 <div class="card card-item card-preview" data-tooltip-content="#tooltip_content_{{ $course->id }}">
                     <div class="card-image">
-                        <a href="course-details.html" class="d-block">
-                            <img class="card-img-top lazy" src="https://management.webeducatorz.com/storage/app/public/{{ $course->thumbnail }}" data-src="images/img8.jpg" alt="Card image cap">
+                        <a href="{{ route('course',$course->slug) }}" class="d-block">
+                            <img class="card-img-top lazy" src="https://management.webeducatorz.com/storage/app/public/{{ $course->thumbnail }}" data-src="https://management.webeducatorz.com/storage/app/public/{{ $course->thumbnail }}" alt="Card image cap">
                         </a>
                         <div class="course-badge-labels">
                             <div class="course-badge">{{ ucfirst($course->category->category) }}</div>
