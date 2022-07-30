a0b01476edc0a581489bed61fe0f6e81ee17bde0
zeeshan6921 <zeeshanishtiaq6921@gmail.com>
2022-07-30 06:06:09 -0700

    lesson

--- resources/views/lms/lesson-details.blade.php
+++ resources/views/lms/lesson-details.blade.php
@@ -1735,20 +1735,20 @@
 </div><!-- end modal -->
 
 <!-- template js files -->
-<script src="{{(asset'js/jquery-3.4.1.min.js')}}"></script>
-<script src="{{(asset'js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{(asset'js/bootstrap-select.min.js')}}"></script>
-<script src="{{(asset'js/owl.carousel.min.js')}}"></script>
-<script src="{{(asset'js/isotope.js')}}"></script>
-<script src="{{(asset'js/waypoint.min.js')}}"></script>
-<script src="{{(asset'js/jquery.counterup.min.js')}}"></script>
-<script src="{{(asset'js/fancybox.js')}}"></script>
-<script src="{{(asset'js/plyr.js')}}"></script>
-<script src="{{(asset'js/datedropper.min.js')}}"></script>
-<script src="{{(asset'js/emojionearea.min.js')}}"></script>
-<script src="{{(asset'js/jquery-te-1.4.0.min.js')}}"></script>
-<script src="{{(asset'js/jquery.MultiFile.min.js')}}"></script>
-<script src="{{(asset'js/main.js')}}"></script>
+<script src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
+<script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('js/bootstrap-select.min.js')}}"></script>
+<script src="{{asset('js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('js/isotope.js')}}"></script>
+<script src="{{asset('js/waypoint.min.js')}}"></script>
+<script src="{{asset('js/jquery.counterup.min.js')}}"></script>
+<script src="{{asset('js/fancybox.js')}}"></script>
+<script src="{{asset('js/plyr.js')}}"></script>
+<script src="{{asset('js/datedropper.min.js')}}"></script>
+<script src="{{asset('js/emojionearea.min.js')}}"></script>
+<script src="{{asset('js/jquery-te-1.4.0.min.js')}}"></script>
+<script src="{{asset('js/jquery.MultiFile.min.js')}}"></script>
+<script src="{{asset('js/main.js')}}"></script>
 <script>
     var player = new Plyr('#player');
 </script>
