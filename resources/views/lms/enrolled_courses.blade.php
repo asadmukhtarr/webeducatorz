698da26933ef09b4bccbeb8c3716049c2079453a
axad03213 <axad03213@gmail.com>
2022-07-30 16:02:53 -0700

    working with enrolled courses

--- resources/views/lms/enrolled_courses.blade.php
+++ resources/views/lms/enrolled_courses.blade.php
@@ -27,7 +27,29 @@
             <div class="tab-content" id="myTabContent">
                 <div class="tab-pane fade active show" id="active-course" role="tabpanel" aria-labelledby="active-course-tab">
                     <div class="row">
-                        <div class="col-lg-4 responsive-column-half">
+                        @foreach($courses as $course)
+                            @php
+                                $c = App\Models\course::where('badge_id',$course->badge_id)->get();
+                            @endphp
+                            <div class="col-lg-4 responsive-column-half">
+                                <div class="card card-item">
+                                    <div class="card-image">
+                                        <a href="lesson-details.html" class="d-block">
+                                            <img class="card-img-top" src="https://management.webeducatorz.com/storage/app/public/{{ $c->picture }}" alt="Card image cap" />
+                                        </a>
+                                    </div><!-- end card-image -->
+                                    <div class="card-body">
+                                        <h5 class="card-title"><a href="lesson-details.html">{{ ucfirst($c->title) }}</a></h5>
+                                        <p class="card-text lh-22 pt-2"><a href="#">{{  $c->category->category }}</a><span>, Software Engineer and Developer</span></p>
+                                    </div><!-- end card-body -->
+                                </div><!-- end card -->
+                            </div>
+                        @endforeach
+                    </div><!-- end row -->
+                </div><!-- end tab-pane -->
+                <div class="tab-pane fade" id="completed-course" role="tabpanel" aria-labelledby="completed-course-tab">
+                    <div class="row">
+                    <div class="col-lg-4 responsive-column-half">
                             <div class="card card-item">
                                 <div class="card-image">
                                     <a href="lesson-details.html" class="d-block">
@@ -37,63 +59,6 @@
                                 <div class="card-body">
                                     <h5 class="card-title"><a href="lesson-details.html">Complete Microsoft SQL Server 2021 for Everyone</a></h5>
                                     <p class="card-text lh-22 pt-2"><a href="teacher-detail.html">Jose Portilla</a><span>, Software Engineer and Developer</span></p>
-                                </div><!-- end card-body -->
-                            </div><!-- end card -->
-                        </div><!-- end col-lg-4 -->
-                    </div><!-- end row -->
-                </div><!-- end tab-pane -->
-                <div class="tab-pane fade" id="completed-course" role="tabpanel" aria-labelledby="completed-course-tab">
-                    <div class="row">
-                        <div class="col-lg-4 responsive-column-half">
-                            <div class="card card-item">
-                                <div class="card-image">
-                                    <a href="lesson-details.html" class="d-block">
-                                        <img class="card-img-top" src="images/img8.jpg" alt="Card image cap">
-                                        <div class="play-button">
-                                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="-307.4 338.8 91.8 91.8" xml:space="preserve">
-                                                        <style type="text/css">
-                                                            .st0{opacity:0.6; fill:#000000; border-radius: 100px;}
-                                                            .st1{fill:#FFFFFF;}
-                                                        </style>
-                                                <g>
-                                                            <circle class="st0" cx="-261.5" cy="384.7" r="45.9"></circle><path class="st1" d="M-272.9,363.2l35.8,20.7c0.7,0.4,0.7,1.3,0,1.7l-35.8,20.7c-0.7,0.4-1.5-0.1-1.5-0.9V364C-274.4,363.3-273.5,362.8-272.9,363.2z"></path>
-                                                        </g>
-                                                    </svg>
-                                        </div>
-                                    </a>
-                                </div><!-- end card-image -->
-                                <div class="card-body">
-                                    <h5 class="card-title"><a href="lesson-details.html">The Complete Full-Stack JavaScript Course!</a></h5>
-                                    <p class="card-text lh-22 pt-2"><a href="teacher-detail.html">Jose Portilla</a><span>, Software Engineer and Developer</span></p>
-                                    <ul class="card-duration d-flex align-items-center fs-15 pt-3">
-                                        <li class="mr-2">
-                                            <span class="text-black">Total Courses:</span>
-                                            <span>5</span>
-                                        </li>
-                                        <li class="mr-2">
-                                            <span class="text-black">Completed Courses:</span>
-                                            <span>5/5</span>
-                                        </li>
-                                    </ul>
-                                    <div class="my-course-progress-bar-wrap d-flex align-items-center pt-3">
-                                        <p class="skillbar-title">Complete:</p>
-                                        <div class="skillbar-box">
-                                            <div class="skillbar skillbar-skillbar-2" data-percent="100%">
-                                                <div class="skillbar-bar skillbar--bar-2 bg-1"></div>
-                                            </div><!-- End Skill Bar -->
-                                        </div>
-                                        <div class="skill-bar-percent">100%</div>
-                                    </div><!-- end my-course-progress-bar-wrap -->
-                                    <div class="rating-wrap d-flex align-items-center justify-content-between pt-3">
-                                        <div class="review-stars">
-                                            <span class="la la-star-o"></span>
-                                            <span class="la la-star-o"></span>
-                                            <span class="la la-star-o"></span>
-                                            <span class="la la-star-o"></span>
-                                            <span class="la la-star-o"></span>
-                                        </div>
-                                        <a href="#" class="btn theme-btn theme-btn-sm theme-btn-transparent" data-toggle="modal" data-target="#ratingModal">Leave a rating</a>
-                                    </div><!-- end rating-wrap -->
                                 </div><!-- end card-body -->
                             </div><!-- end card -->
                         </div><!-- end col-lg-4 -->
