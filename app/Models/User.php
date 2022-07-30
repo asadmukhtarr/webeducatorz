33c26524a3a559f75f923760d30b53cdc777b41c
zeeshan6921 <zeeshanishtiaq6921@gmail.com>
2022-07-30 05:25:28 -0700

    lesson

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function student(){
+        return $this->hasMany(student::class);
+    }
 }
