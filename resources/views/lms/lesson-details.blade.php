6766683c4b2eb63707f6392be599c77ab1a93b6e
zeeshan6921 <zeeshanishtiaq6921@gmail.com>
2022-07-30 06:46:16 -0700

    video

--- resources/views/lms/lesson-details.blade.php
+++ resources/views/lms/lesson-details.blade.php
@@ -49,28 +49,6 @@
     <div class="header-menu-content bg-dark">
         <div class="container-fluid">
             <div class="main-menu-content d-flex align-items-center">
-                <div class="logo-box logo--box">
-                    <div class="theme-picker d-flex align-items-center">
-                        <button class="theme-picker-btn dark-mode-btn" title="Dark mode">
-                            <svg class="svg-icon-color-white" viewBox="0 0 24 24" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-                            </svg>
-                        </button>
-                        <button class="theme-picker-btn light-mode-btn" title="Light mode">
-                            <svg viewBox="0 0 24 24" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                                <circle cx="12" cy="12" r="5"></circle>
-                                <line x1="12" y1="1" x2="12" y2="3"></line>
-                                <line x1="12" y1="21" x2="12" y2="23"></line>
-                                <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-                                <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-                                <line x1="1" y1="12" x2="3" y2="12"></line>
-                                <line x1="21" y1="12" x2="23" y2="12"></line>
-                                <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-                                <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-                            </svg>
-                        </button>
-                    </div>
-                </div><!-- end logo-box -->
                 <div class="course-dashboard-header-title pl-4">
                     <a href="#" class="text-white fs-15">Java Programming Masterclass for Software Developers</a>
                 </div><!-- end course-dashboard-header-title -->
@@ -105,34 +83,7 @@
                             <!-- Caption files -->
                             <track kind="captions" label="English" srclang="en" src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.en.vtt" default/>
                             <track kind="captions" label="Français" srclang="fr" src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.fr.vtt"/>
-
-                            <!-- Fallback for browsers that don't support the <video> element -->
-                            <a href="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4" download>Download</a>
                         </video>
-                    </div>
-                    <div class="lecture-viewer-text-wrap">
-                        <div class="lecture-viewer-text-content custom-scrollbar-styled">
-                            <div class="lecture-viewer-text-body">
-                                <h2 class="fs-24 font-weight-semi-bold pb-4">Download your Footage for your Quick Start</h2>
-                                <div class="lecture-viewer-content-detail">
-                                    <ul class="generic-list-item pb-4">
-                                        <li>Hi</li>
-                                        <li>Welcome to Motion Graphics in After Effects. </li>
-                                        <li>In the next lectures you will start creating your first animation and animate imported footage.</li>
-                                        <li>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes,</li>
-                                        <li>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. </li>
-                                        <li>Occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. </li>
-                                        <li>Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus,</li>
-                                        <li>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. </li>
-                                        <li><strong class="font-weight-semi-bold">Download your footage Now, Click on the Link Below.</strong></li>
-                                    </ul>
-                                    <div class="btn-box">
-                                        <h3 class="fs-18 font-weight-semi-bold pb-3">Resources for this lecture</h3>
-                                        <a href="#" class="btn theme-btn theme-btn-transparent"><i class="la la-file-zip-o mr-1"></i>Quick-start.zip</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div><!-- end lecture-viewer-container -->
             </div><!-- end course-dashboard-column -->
@@ -147,7 +98,7 @@
                         <div class="accordion generic-accordion generic--accordion" id="accordionCourseExample">
                             <div class="card">
                                 <div class="card-header" id="headingOne">
-                                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    <button class="btn btn-link" type="button">
                                         <span class="fs-15"> Section 1: Dive in and Discover After Effects</span>
                                     </button>
                                 </div><!-- end card-header -->
@@ -169,181 +120,6 @@
 </div> --}}
 <!-- end scroll top -->
 
-<!-- Modal -->
-<div class="modal fade modal-container" id="ratingModal" tabindex="-1" role="dialog" aria-labelledby="ratingModalTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header border-bottom-gray">
-                <div class="pr-2">
-                    <h5 class="modal-title fs-19 font-weight-semi-bold lh-24" id="ratingModalTitle">
-                        How would you rate this course?
-                    </h5>
-                </div>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true" class="la la-times"></span>
-                </button>
-            </div><!-- end modal-header -->
-            <div class="modal-body text-center py-5">
-                <div class="leave-rating mt-5">
-                    <input type="radio" name='rate' id="star5"/>
-                    <label for="star5" class="fs-45"></label>
-                    <input type="radio" name='rate' id="star4"/>
-                    <label for="star4" class="fs-45"></label>
-                    <input type="radio" name='rate' id="star3"/>
-                    <label for="star3" class="fs-45"></label>
-                    <input type="radio" name='rate' id="star2"/>
-                    <label for="star2" class="fs-45"></label>
-                    <input type="radio" name='rate' id="star1"/>
-                    <label for="star1" class="fs-45"></label>
-                    <div class="rating-result-text fs-20 pb-4"></div>
-                </div><!-- end leave-rating -->
-            </div><!-- end modal-body -->
-        </div><!-- end modal-content -->
-    </div><!-- end modal-dialog -->
-</div><!-- end modal -->
-
-<!-- Modal -->
-<div class="modal fade modal-container" id="shareModal" tabindex="-1" role="dialog" aria-labelledby="shareModalTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header border-bottom-gray">
-                <h5 class="modal-title fs-19 font-weight-semi-bold" id="shareModalTitle">Share this course</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true" class="la la-times"></span>
-                </button>
-            </div><!-- end modal-header -->
-            <div class="modal-body">
-                <div class="copy-to-clipboard">
-                    <span class="success-message">Copied!</span>
-                    <div class="input-group">
-                        <input type="text" class="form-control form--control copy-input pl-3" value="https://www.aduca.com/share/101WxMB0oac1hVQQ==/">
-                        <div class="input-group-append">
-                            <button class="btn theme-btn theme-btn-sm copy-btn shadow-none"><i class="la la-copy mr-1"></i> Copy</button>
-                        </div>
-                    </div>
-                </div><!-- end copy-to-clipboard -->
-            </div><!-- end modal-body -->
-            <div class="modal-footer justify-content-center border-top-gray">
-                <ul class="social-icons social-icons-styled">
-                    <li><a href="#" class="facebook-bg"><i class="la la-facebook"></i></a></li>
-                    <li><a href="#" class="twitter-bg"><i class="la la-twitter"></i></a></li>
-                    <li><a href="#" class="instagram-bg"><i class="la la-instagram"></i></a></li>
-                </ul>
-            </div><!-- end modal-footer -->
-        </div><!-- end modal-content-->
-    </div><!-- end modal-dialog -->
-</div><!-- end modal -->
-
-<!-- Modal -->
-<div class="modal fade modal-container" id="reportModal" tabindex="-1" role="dialog" aria-labelledby="reportModalTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header border-bottom-gray">
-                <div class="pr-2">
-                    <h5 class="modal-title fs-19 font-weight-semi-bold lh-24" id="reportModalTitle">Report Abuse</h5>
-                    <p class="pt-1 fs-14 lh-24">Flagged content is reviewed by Aduca staff to determine whether it violates Terms of Service or Community Guidelines. If you have a question or technical issue, please contact our
-                        <a href="contact.html" class="text-color hover-underline">Support team here</a>.</p>
-                </div>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true" class="la la-times"></span>
-                </button>
-            </div><!-- end modal-header -->
-            <div class="modal-body">
-                <form method="post">
-                    <div class="input-box">
-                        <label class="label-text">Select Report Type</label>
-                        <div class="form-group">
-                            <div class="select-container w-auto">
-                                <select class="select-container-select">
-                                    <option value>-- Select One --</option>
-                                    <option value="1">Inappropriate Course Content</option>
-                                    <option value="2">Inappropriate Behavior</option>
-                                    <option value="3">Aduca Policy Violation</option>
-                                    <option value="4">Spammy Content</option>
-                                    <option value="5">Other</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="input-box">
-                        <label class="label-text">Write Message</label>
-                        <div class="form-group">
-                            <textarea class="form-control form--control pl-3" name="message" placeholder="Provide additional details here..." rows="5"></textarea>
-                        </div>
-                    </div>
-                    <div class="btn-box text-right pt-2">
-                        <button type="button" class="btn font-weight-medium mr-3" data-dismiss="modal">Cancel</button>
-                        <button type="submit" class="btn theme-btn theme-btn-sm lh-30">Submit <i class="la la-arrow-right icon ml-1"></i></button>
-                    </div>
-                </form>
-            </div><!-- end modal-body -->
-        </div><!-- end modal-content -->
-    </div><!-- end modal-dialog -->
-</div><!-- end modal -->
-
-<!-- Modal -->
-<div class="modal fade modal-container" id="insertLinkModal" tabindex="-1" role="dialog" aria-labelledby="insertLinkModalTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header border-bottom-gray">
-                <div class="pr-2">
-                    <h5 class="modal-title fs-19 font-weight-semi-bold lh-24" id="insertLinkModalTitle">Insert Link</h5>
-                </div>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true" class="la la-times"></span>
-                </button>
-            </div><!-- end modal-header -->
-            <div class="modal-body">
-                <form action="#">
-                    <div class="input-box">
-                        <label class="label-text">URL</label>
-                        <div class="form-group">
-                            <input class="form-control form--control" type="text" name="text" placeholder="Url">
-                            <i class="la la-link input-icon"></i>
-                        </div>
-                    </div>
-                    <div class="input-box">
-                        <label class="label-text">Text</label>
-                        <div class="form-group">
-                            <input class="form-control form--control" type="text" name="text" placeholder="Text">
-                            <i class="la la-pencil input-icon"></i>
-                        </div>
-                    </div>
-                    <div class="btn-box text-right pt-2">
-                        <button type="button" class="btn font-weight-medium mr-3" data-dismiss="modal">Cancel</button>
-                        <button type="submit" class="btn theme-btn theme-btn-sm lh-30">Insert <i class="la la-arrow-right icon ml-1"></i></button>
-                    </div>
-                </form>
-            </div><!-- end modal-body -->
-        </div><!-- end modal-content -->
-    </div><!-- end modal-dialog -->
-</div><!-- end modal -->
-
-<!-- Modal -->
-<div class="modal fade modal-container" id="uploadPhotoModal" tabindex="-1" role="dialog" aria-labelledby="uploadPhotoModalTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header border-bottom-gray">
-                <div class="pr-2">
-                    <h5 class="modal-title fs-19 font-weight-semi-bold lh-24" id="uploadPhotoModalTitle">Upload an Image</h5>
-                </div>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true" class="la la-times"></span>
-                </button>
-            </div><!-- end modal-header -->
-            <div class="modal-body">
-                <div class="file-upload-wrap">
-                    <input type="file" name="files[]" class="multi file-upload-input" multiple>
-                    <span class="file-upload-text"><i class="la la-upload mr-2"></i>Drop files here or click to upload</span>
-                </div><!-- file-upload-wrap -->
-                <div class="btn-box text-right pt-2">
-                    <button type="button" class="btn font-weight-medium mr-3" data-dismiss="modal">Cancel</button>
-                    <button type="submit" class="btn theme-btn theme-btn-sm lh-30">Submit <i class="la la-arrow-right icon ml-1"></i></button>
-                </div>
-            </div><!-- end modal-body -->
-        </div><!-- end modal-content -->
-    </div><!-- end modal-dialog -->
-</div><!-- end modal -->
 
 <!-- template js files -->
 <script src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
