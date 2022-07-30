e139db9a9f3c136f992340b1bfbe1c800f833f2a
zeeshan6921 <zeeshanishtiaq6921@gmail.com>
2022-07-30 06:04:10 -0700

    lesson

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\TrailForm;
+use App\Models\general;
 use Auth;
 
 class DashboardController extends Controller
