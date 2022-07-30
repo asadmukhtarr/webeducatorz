6506a3521167942e4309ac2eb9673db652361b0f
zeeshan6921 <zeeshanishtiaq6921@gmail.com>
2022-07-30 05:46:58 -0700

    lesson

--- resources/views/lms/lesson-details.blade.php
+++ resources/views/lms/lesson-details.blade.php
@@ -9,28 +9,6 @@
         <div class="header-menu-content bg-dark">
             <div class="container-fluid">
                 <div class="main-menu-content d-flex align-items-center">
-                    <div class="logo-box logo--box">
-                        <div class="theme-picker d-flex align-items-center">
-                            <button class="theme-picker-btn dark-mode-btn" title="Dark mode">
-                                <svg class="svg-icon-color-white" viewBox="0 0 24 24" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                                    <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-                                </svg>
-                            </button>
-                            <button class="theme-picker-btn light-mode-btn" title="Light mode">
-                                <svg viewBox="0 0 24 24" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                                    <circle cx="12" cy="12" r="5"></circle>
-                                    <line x1="12" y1="1" x2="12" y2="3"></line>
-                                    <line x1="12" y1="21" x2="12" y2="23"></line>
-                                    <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-                                    <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-                                    <line x1="1" y1="12" x2="3" y2="12"></line>
-                                    <line x1="21" y1="12" x2="23" y2="12"></line>
-                                    <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-                                    <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-                                </svg>
-                            </button>
-                        </div>
-                    </div><!-- end logo-box -->
                     <div class="course-dashboard-header-title pl-4">
                         <a href="course-details.html" class="text-white fs-15">Java Programming Masterclass for Software Developers</a>
                     </div><!-- end course-dashboard-header-title -->
