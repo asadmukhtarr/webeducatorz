fdca31fabc35aa12b8ef670029ab3b2c137455e0
axad03213 <axad03213@gmail.com>
2022-07-30 17:22:49 -0700

    testing enrollment

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,7 +18,7 @@ class DashboardController extends Controller
     }
 
     public function enrolled_courses(){
-        $courses =  Auth::user()->student->enrollment;
+        $courses =  Auth::user()->student;
         return $courses;
         return view('lms.enrolled_courses',compact('courses'));
     }
