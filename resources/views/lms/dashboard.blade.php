975b0352bda0acce77f9bbbeb349709c14b5400d
zeeshan6921 <zeeshanishtiaq6921@gmail.com>
2022-08-01 00:03:44 -0700

    lms

--- resources/views/lms/dashboard.blade.php
+++ resources/views/lms/dashboard.blade.php
@@ -82,8 +82,8 @@
                             @endforeach
                             
                             <div class="pl-4">
-                                <p class="card-text fs-18">{{$b::where('status',1)->count()}}Active Courses</p>
-                                <h5 class="card-title pt-2 fs-26">5</h5>
+                                <p class="card-text fs-18">Active Courses</p>
+                                <h5 class="card-title pt-2 fs-26">{{$b::where('status',1)->count()}}</h5>
                             </div>
                         </div><!-- end card-body -->
                     </div><!-- end card -->
@@ -127,7 +127,7 @@
                             </div>
                             <div class="pl-4">
                                 <p class="card-text fs-18">Completed Courses</p>
-                                <h5 class="card-title pt-2 fs-26">6</h5>
+                                <h5 class="card-title pt-2 fs-26">{{$b::where('status',0)->count()}}</h5>
                             </div>
                         </div><!-- end card-body -->
                     </div><!-- end card -->
