962e4dbd80b6b60df8fc0674bae37d427534faac
zeeshan6921 <zeeshanishtiaq6921@gmail.com>
2022-08-01 02:49:38 -0700

    lms

--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -558,7 +558,7 @@
     <script>
         $(document).ready(function(){
             $("[add='change']").on("click",function(){
-                $(this).addClass("page-active");
+                $("[add='change']").addClass("page-active");
             });
         });
     </script>
