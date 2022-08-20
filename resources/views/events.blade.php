bfb58a9429caf8900f50c1222473fb9d9e9e65b0
zeeshan6921 <zeeshanishtiaq6921@gmail.com>
2022-08-20 01:23:12 -0700

    workshop

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -9,25 +9,38 @@
                         <table class="table table-striped table-bordered table-hover">
                             <tr class="table-bg fee-table" style="color:white; background-color:#FF0302;">
                                 <th>Upcoming Workshops</th>
-                                <th>Date</th>
-                                <th>Timing</th>
+                                <th>Type</th>
                                 <th>Category</th>
-                                <th>Enroll Now</th>
+                                <th>Date</th>
+                                <th>Time</th>
+								<th>Register Link</th>
+								<th>Meeting Link</th>
+								<th>Status</th>
                             </tr>
                             @foreach ($events as $event)
                                 <tr>
                                     <td>{{ ucfirst($event->title) }}</td>
+									<td>
+                                        {{ ucfirst($event->type) }}
+                                    </td>
+									<td>
+                                        {{ ucfirst($event->category->category) }}
+                                    </td>
                                     <td>
                                         {{ $event->date }}
                                     </td>
                                     <td>
                                         {{ date("g:i a", strtotime($event->start)) }} -   {{ date("g:i a", strtotime($event->end)) }}
                                     </td>
+									<td>
+										<a class="btn btn-sm btn-outline-primary" href="{{$event->reg_link}}">Register</a>
+									</td>
+									<td>
+										<a class="btn btn-sm btn-outline-primary" href="{{$event->meeting}}">Meeting Link</a>
+										
+									</td>
                                     <td>
-                                        {{ ucfirst($event->category->category) }}
-                                    </td>
-                                    <td>
-                                        <button class="btn theme-bg btn-rounded">Details</button>
+                                        <span class="label label-primary">{{ucfirst($event->status)}}</span>
                                     </td>
 
                                 </tr>
