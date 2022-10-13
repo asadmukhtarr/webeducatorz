11c56cead2796d870b703aafeb68aaa0b33a7a48
axad03213 <axad03213@gmail.com>
2022-10-13 06:01:50 -0700

    updates

--- resources/views/layouts/newlms/app.blade.php
+++ resources/views/layouts/newlms/app.blade.php
@@ -8,33 +8,33 @@
 
 	<!-- Global stylesheets -->
 	<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
-	<link href="{{ asset('lms/global_assets/css/icons/icomoon/styles.min.css') }}" rel="stylesheet" type="text/css">
-	<link href="{{ asset('lms/assets/css/all.min.css') }}" rel="stylesheet" type="text/css">
+	<link href="{{ asset('public/lms/global_assets/css/icons/icomoon/styles.min.css') }}" rel="stylesheet" type="text/css">
+	<link href="{{ asset('public/lms/assets/css/all.min.css') }}" rel="stylesheet" type="text/css">
 	<!-- /global stylesheets -->
 
 	<!-- Core JS files -->
-	<script src="{{ asset('lms/global_assets/js/main/jquery.min.js') }}"></script>
-	<script src="{{ asset('lms/global_assets/js/main/bootstrap.bundle.min.js') }}"></script>
+	<script src="{{ asset('public/lms/global_assets/js/main/jquery.min.js') }}"></script>
+	<script src="{{ asset('public/lms/global_assets/js/main/bootstrap.bundle.min.js') }}"></script>
 	<!-- /core JS files -->
 
 	<!-- Theme JS files -->
-	<script src="{{ asset('lms/global_assets/js/plugins/visualization/d3/d3.min.js') }}"></script>
-	<script src="{{ asset('lms/global_assets/js/plugins/visualization/d3/d3_tooltip.js') }}"></script>
-	<script src="{{ asset('lms/global_assets/js/plugins/ui/moment/moment.min.js') }}"></script>
-	<script src="{{ asset('lms/global_assets/js/plugins/pickers/daterangepicker.js') }}"></script>
+	<script src="{{ asset('public/lms/global_assets/js/plugins/visualization/d3/d3.min.js') }}"></script>
+	<script src="{{ asset('public/lms/global_assets/js/plugins/visualization/d3/d3_tooltip.js') }}"></script>
+	<script src="{{ asset('public/lms/global_assets/js/plugins/ui/moment/moment.min.js') }}"></script>
+	<script src="{{ asset('public/lms/global_assets/js/plugins/pickers/daterangepicker.js') }}"></script>
 
-	<script src="{{ asset('lms/assets/js/app.js') }}"></script>
-	<script src="{{ asset('lms/global_assets/js/demo_pages/dashboard.js') }}"></script>
-	<script src="{{ asset('lms/global_assets/js/demo_charts/pages/dashboard/light/streamgraph.js') }}"></script>
-	<script src="{{ asset('lms/global_assets/js/demo_charts/pages/dashboard/light/sparklines.js') }}"></script>
-	<script src="{{ asset('lms/global_assets/js/demo_charts/pages/dashboard/light/lines.js') }}"></script>	
-	<script src="{{ asset('lms/global_assets/js/demo_charts/pages/dashboard/light/areas.js') }}"></script>
-	<script src="{{ asset('lms/global_assets/js/demo_charts/pages/dashboard/light/donuts.js') }}"></script>
-	<script src="{{ asset('lms/global_assets/js/demo_charts/pages/dashboard/light/bars.js') }}"></script>
-	<script src="{{ asset('lms/global_assets/js/demo_charts/pages/dashboard/light/progress.js') }}"></script>
-	<script src="{{ asset('lms/global_assets/js/demo_charts/pages/dashboard/light/heatmaps.js') }}"></script>
-	<script src="{{ asset('lms/global_assets/js/demo_charts/pages/dashboard/light/pies.js') }}"></script>
-	<script src="{{ asset('lms/global_assets/js/demo_charts/pages/dashboard/light/bullets.js') }}"></script>
+	<script src="{{ asset('public/lms/assets/js/app.js') }}"></script>
+	<script src="{{ asset('public/lms/global_assets/js/demo_pages/dashboard.js') }}"></script>
+	<script src="{{ asset('public/lms/global_assets/js/demo_charts/pages/dashboard/light/streamgraph.js') }}"></script>
+	<script src="{{ asset('public/lms/global_assets/js/demo_charts/pages/dashboard/light/sparklines.js') }}"></script>
+	<script src="{{ asset('public/lms/global_assets/js/demo_charts/pages/dashboard/light/lines.js') }}"></script>	
+	<script src="{{ asset('public/lms/global_assets/js/demo_charts/pages/dashboard/light/areas.js') }}"></script>
+	<script src="{{ asset('public/lms/global_assets/js/demo_charts/pages/dashboard/light/donuts.js') }}"></script>
+	<script src="{{ asset('public/lms/global_assets/js/demo_charts/pages/dashboard/light/bars.js') }}"></script>
+	<script src="{{ asset('public/lms/global_assets/js/demo_charts/pages/dashboard/light/progress.js') }}"></script>
+	<script src="{{ asset('public/lms/global_assets/js/demo_charts/pages/dashboard/light/heatmaps.js') }}"></script>
+	<script src="{{ asset('public/lms/global_assets/js/demo_charts/pages/dashboard/light/pies.js') }}"></script>
+	<script src="{{ asset('public/lms/global_assets/js/demo_charts/pages/dashboard/light/bullets.js') }}"></script>
 	<!-- /theme JS files -->
 
 </head>
